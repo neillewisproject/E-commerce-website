827c33a6cb07cdd6c5a622df5ce99bdc1d7d72af
Neil Lewis <neil.lewis@alpicloud.onmicrosoft.com>
2023-11-29 18:39:05 +0530

    add all changes

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ["res.cloudinary.com", "www.w3.org", "sttoolsprdwe1217.blob.core.windows.net"],
+  },
+};
+
+module.exports = nextConfig;
+
